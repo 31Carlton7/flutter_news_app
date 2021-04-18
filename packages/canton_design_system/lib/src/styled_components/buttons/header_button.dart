d60fa6ced3604846f76cfa5ddd8a6209660c78bd
31Carlton7 <carltonaikins7@gmail.com>
2021-04-17 21:40:51 -0400

    add size(double)

--- packages/canton_design_system/lib/src/styled_components/buttons/header_button.dart
+++ packages/canton_design_system/lib/src/styled_components/buttons/header_button.dart
@@ -8,18 +8,19 @@ class CantonHeaderButton extends StatelessWidget {
     this.icon,
     this.onPressed,
     this.radius,
+    this.size,
   }) : super(key: key);
   final void Function() onPressed;
   final IconData icon;
   final Color backgroundColor, iconColor;
-  final double radius;
+  final double radius, size;
 
   @override
   Widget build(BuildContext context) {
     return CantonPrimaryButton(
       containerColor: backgroundColor ?? cantonGrey[200],
-      containerHeight: 60.0,
-      containerWidth: 60.0,
+      containerHeight: size ?? 60.0,
+      containerWidth: size ?? 60.0,
       radius: radius ?? 50,
       containerPadding: const EdgeInsets.all(0),
       prefixIcon: icon,
