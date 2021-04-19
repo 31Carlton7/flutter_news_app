a99cf21701f6befb81a42ef3a26e2e31be045e21
31Carlton7 <carltonaikins7@gmail.com>
2021-04-19 17:54:01 -0400

    add Source model

---
+++ lib/src/models/source.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+class Source {
+  String id;
+  String name;
+  String category;
+  String country;
+
+  Source({this.id, this.name, this.category, this.country});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'category': category,
+      'country': country,
+    };
+  }
+
+  factory Source.fromMap(Map<String, dynamic> map) {
+    return Source(
+      id: map['id'] as String,
+      name: map['name'] as String,
+      category: map['category'] as String,
+      country: map['country'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Source.fromJson(String source) => Source.fromMap(json.decode(source));
+}
