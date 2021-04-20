49caea2933a8c1eac066b177a7cec14854254cba
31Carlton7 <carltonaikins7@gmail.com>
2021-04-20 10:39:27 -0400

    add 'if null'

--- lib/src/ui/providers/news_future_provider.dart
+++ lib/src/ui/providers/news_future_provider.dart
@@ -1,12 +1,21 @@
+import 'package:canton_news_app/src/config/environment_config.dart';
 import 'package:canton_news_app/src/models/article.dart';
 import 'package:canton_news_app/src/ui/providers/news_service_provider.dart';
 import 'package:riverpod/riverpod.dart';
+
+String futurePath;
+final _environmentConfig = EnvironmentConfig();
 
 final newsFutureProvider =
     FutureProvider.autoDispose<List<Article>>((ref) async {
   ref.maintainState = true;
 
   final newsService = ref.read(newsServiceProvider);
-  final articles = newsService.getArticles();
+
+  final articles = newsService.getArticles(
+    // hdlines: headlines ?? true,
+    path: futurePath ??
+        'https://newsapi.org/v2/top-headlines?language=en&apiKey=${_environmentConfig.newsApiKey}',
+  );
   return articles;
 });
