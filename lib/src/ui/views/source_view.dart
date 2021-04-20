f079d3c70341d7d75c5328cb514157578ccb4e6f
31Carlton7 <carltonaikins7@gmail.com>
2021-04-20 10:41:10 -0400

    add 'onInit' & 'onDispose'

--- lib/src/ui/views/source_view.dart
+++ lib/src/ui/views/source_view.dart
@@ -1,52 +1,70 @@
 import 'package:canton_design_system/canton_design_system.dart';
 import 'package:canton_news_app/src/config/environment_config.dart';
 import 'package:canton_news_app/src/config/news_exceptions.dart';
-import 'package:canton_news_app/src/models/article.dart';
 import 'package:canton_news_app/src/models/source.dart';
 import 'package:canton_news_app/src/ui/providers/news_future_provider.dart';
-import 'package:canton_news_app/src/ui/providers/news_sources_future_provider.dart';
-import 'package:canton_news_app/src/ui/styled_components/article_card_large.dart';
-import 'package:canton_news_app/src/ui/styled_components/article_card_medium.dart';
 import 'package:canton_news_app/src/ui/styled_components/article_grid.dart';
 import 'package:canton_news_app/src/ui/styled_components/article_list.dart';
 import 'package:canton_news_app/src/ui/styled_components/error_body.dart';
 import 'package:canton_news_app/src/ui/styled_components/unexpected_error.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SourceView extends ConsumerWidget {
+class SourceView extends StatefulWidget {
   const SourceView(this.source);
   @required
   final Source source;
   @override
-  Widget build(BuildContext context, ScopedReader watch) {
+  _SourceViewState createState() => _SourceViewState();
+}
+
+class _SourceViewState extends State<SourceView> {
+  @override
+  void initState() {
+    super.initState();
+    final _environmentConfig = EnvironmentConfig();
+    final now = DateTime.now();
+
+    /// Only allows articles from the last week to be returned.
+    futurePath =
+        'https://newsapi.org/v2/everything?qInTitle=${widget.source.name}&language=en&from=${DateTime(now.year, now.month, now.day - 7)}&apiKey=${_environmentConfig.newsApiKey}';
+    context.refresh(newsFutureProvider);
+  }
+
+  @override
+  void dispose() {
+    futurePath = null;
+    context.refresh(newsFutureProvider);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return CantonScaffold(
-      body: _content(context, watch),
+      body: _content(context),
     );
   }
 
-  Widget _content(BuildContext context, ScopedReader watch) {
-    final _environmentConfig = EnvironmentConfig();
-    // path =
-    //     'https://newsapi.org/v2/sources?category=${source.name}&apiKey=${_environmentConfig.newsApiKey}';
-    return watch(newsSourcesFutureProvider).when(
-      loading: () => Loading(),
-      error: (e, s) {
-        if (e is NewsExceptions) {
-          return ErrorBody(e.message);
-        }
-        return UnexpectedError(newsSourcesFutureProvider);
-      },
-      data: (articles) {
-        return CustomScrollView(
-          slivers: <Widget>[
-            _header(context),
-            // ArticleList(articles),
-            // ArticleGrid(articles),
-            // _articleList(context, articles),
-          ],
-        );
-      },
-    );
+  Widget _content(BuildContext context) {
+    return Consumer(builder: (context, watch, child) {
+      return watch(newsFutureProvider).when(
+        loading: () => Loading(),
+        error: (e, s) {
+          if (e is NewsExceptions) {
+            return ErrorBody(e.message);
+          }
+          return UnexpectedError(newsFutureProvider);
+        },
+        data: (articles) {
+          return CustomScrollView(
+            slivers: <Widget>[
+              _header(context),
+              ArticleList(articles, false),
+              ArticleGrid(articles),
+            ],
+          );
+        },
+      );
+    });
   }
 
   Widget _header(BuildContext context) {
@@ -61,7 +79,7 @@ class SourceView extends ConsumerWidget {
             isClear: true,
           ),
           Text(
-            source.name,
+            widget.source.name,
             style: Theme.of(context)
                 .textTheme
                 .headline5
@@ -69,40 +87,6 @@ class SourceView extends ConsumerWidget {
           ),
           Container(width: 60),
         ],
-      ),
-    );
-  }
-
-  Widget _articleList(BuildContext context, List<Article> articles) {
-    return Expanded(
-      child: RefreshList(
-        color: cantonSuccess[600],
-        onRefresh: () => context.refresh(newsFutureProvider),
-        child: ListView.separated(
-          /// Sets ItemCount to one incase of error and needs to display on Item in the list
-          /// This also allows the use of the [LiquidPullToRefresh] widget, to refresh the feed.
-          itemCount: articles.length != 0 ? articles.length : 1,
-          separatorBuilder: (context, index) {
-            return SizedBox(height: 5);
-          },
-          itemBuilder: (context, index) {
-            return articles.length != 0
-                ? (index == 0 || index == 4 || index == 9)
-                    ? ArticleCardLarge(articles[index])
-                    : ArticleCardMedium(
-                        articles[index],
-                      )
-                : Center(
-                    child: Text(
-                      'No Articles :(',
-                      style: Theme.of(context)
-                          .textTheme
-                          .headline5
-                          .copyWith(color: cantonGrey[600]),
-                    ),
-                  );
-          },
-        ),
       ),
     );
   }
--- lib/src/ui/views/top_stories_view.dart
+++ lib/src/ui/views/top_stories_view.dart
@@ -1,45 +1,67 @@
 import 'package:canton_design_system/canton_design_system.dart';
+import 'package:canton_news_app/src/config/environment_config.dart';
 import 'package:canton_news_app/src/config/news_exceptions.dart';
 import 'package:canton_news_app/src/ui/providers/news_future_provider.dart';
 import 'package:canton_news_app/src/ui/styled_components/article_grid.dart';
 import 'package:canton_news_app/src/ui/styled_components/article_list.dart';
 import 'package:canton_news_app/src/ui/styled_components/error_body.dart';
 import 'package:canton_news_app/src/ui/styled_components/unexpected_error.dart';
-import 'package:canton_news_app/src/ui/views/category_view.dart';
+import 'package:canton_news_app/src/ui/views/category_list_view.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod/riverpod.dart';
 
-class TopStoriesView extends ConsumerWidget {
+class TopStoriesView extends StatefulWidget {
   @override
-  Widget build(BuildContext context, ScopedReader watch) {
+  _TopStoriesViewState createState() => _TopStoriesViewState();
+}
+
+class _TopStoriesViewState extends State<TopStoriesView> {
+  @override
+  void initState() {
+    super.initState();
+    futurePath = null;
+    context.refresh(newsFutureProvider);
+  }
+
+  @override
+  void dispose() {
+    context.refresh(newsFutureProvider);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return CantonScaffold(
-      body: _content(context, watch),
+      body: _content(context),
     );
   }
 
-  Widget _content(BuildContext context, ScopedReader watch) {
-    return watch(newsFutureProvider).when(
-      error: (e, s) {
-        if (e is NewsExceptions) {
-          return ErrorBody(e.message);
-        }
-        return UnexpectedError(newsFutureProvider);
-      },
-      loading: () => Loading(),
-      data: (articles) {
-        return CustomScrollView(
-          slivers: [
-            _header(context),
-            CupertinoSliverRefreshControl(
-              onRefresh: () async => await context.refresh(newsFutureProvider),
-            ),
-            ArticleList(articles),
-            ArticleGrid(articles),
-          ],
-        );
-      },
-    );
+  Widget _content(BuildContext context) {
+    futurePath = null;
+    return Consumer(builder: (context, watch, child) {
+      return watch(newsFutureProvider).when(
+        error: (e, s) {
+          if (e is NewsExceptions) {
+            return ErrorBody(e.message);
+          }
+          return UnexpectedError(newsFutureProvider);
+        },
+        loading: () => Loading(),
+        data: (articles) {
+          return CustomScrollView(
+            slivers: [
+              _header(context),
+              CupertinoSliverRefreshControl(
+                onRefresh: () async =>
+                    await context.refresh(newsFutureProvider),
+              ),
+              ArticleList(articles, true),
+              ArticleGrid(articles),
+            ],
+          );
+        },
+      );
+    });
   }
 
   Widget _header(BuildContext context) {
@@ -53,7 +75,7 @@ class TopStoriesView extends ConsumerWidget {
           CantonActionButton(
             icon: FeatherIcons.list,
             iconColor: cantonGrey[600],
-            onPressed: () => viewTransition(context, CategoryView()),
+            onPressed: () => viewTransition(context, CategoryListView()),
           ),
           Text(
             'Top Stories',
