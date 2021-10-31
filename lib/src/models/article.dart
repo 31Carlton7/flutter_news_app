423fe7f065ec686e5647afdfc73f69de8b16ca11
Carlton Aikins <carltonaikins7@gmail.com>
2021-10-31 16:11:11 -0400

    Migrate to null safety

--- lib/src/models/article.dart
+++ lib/src/models/article.dart
@@ -1,13 +1,13 @@
 import 'dart:convert';
 
 class Article {
-  String id;
-  String name;
-  String author;
-  String title;
-  String content;
-  String imagePath;
-  String publishedAt;
+  String? id;
+  String? name;
+  String? author;
+  String? title;
+  String? content;
+  String? imagePath;
+  String? publishedAt;
 
   Article({
     this.id,
@@ -22,17 +22,9 @@ class Article {
   String get getImageUrl => '$imagePath';
 
   String get getPublishedAtFormattedTime {
-    return (DateTime.now().difference(DateTime.parse(publishedAt)).inHours < 1)
-        ? DateTime.now()
-                .difference(DateTime.parse(publishedAt))
-                .inMinutes
-                .toString() +
-            'm ago'
-        : DateTime.now()
-                .difference(DateTime.parse(publishedAt))
-                .inHours
-                .toString() +
-            'h ago';
+    return (DateTime.now().difference(DateTime.parse(publishedAt!)).inHours < 1)
+        ? DateTime.now().difference(DateTime.parse(publishedAt!)).inMinutes.toString() + 'm ago'
+        : DateTime.now().difference(DateTime.parse(publishedAt!)).inHours.toString() + 'h ago';
   }
 
   Map<String, dynamic> toMap() {
@@ -48,16 +40,14 @@ class Article {
   }
 
   factory Article.fromMap(Map<String, dynamic> map) {
-    if (map == null) return null;
-
     return Article(
-      id: map['source']['id'] as String,
-      name: map['source']['name'] as String,
-      author: map['author'] as String,
-      title: map['title'] as String,
-      content: map['content'] as String,
-      imagePath: map['urlToImage'] as String,
-      publishedAt: map['publishedAt'] as String,
+      id: map['source']['id'] as String?,
+      name: map['source']['name'] as String?,
+      author: map['author'] as String?,
+      title: map['title'] as String?,
+      content: map['content'] as String?,
+      imagePath: map['urlToImage'] as String?,
+      publishedAt: map['publishedAt'] as String?,
     );
   }
 
