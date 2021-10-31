cf8370bbbf19dcfe55c9e12996155b6c91989fd5
Carlton Aikins <carltonaikins7@gmail.com>
2021-10-31 16:13:45 -0400

    Add complex navigation functionality

--- lib/src/ui/views/current_view.dart
+++ lib/src/ui/views/current_view.dart
@@ -1,7 +1,12 @@
 import 'package:canton_design_system/canton_design_system.dart';
 import 'package:canton_news_app/src/config/bottom_navigation_bar.dart';
-import 'package:canton_news_app/src/ui/views/top_sources_view.dart';
-import 'package:canton_news_app/src/ui/views/top_stories_view.dart';
+import 'package:canton_news_app/src/ui/views/category_list_view/category_list_view.dart';
+import 'package:canton_news_app/src/ui/views/sources_view/sources_view.dart';
+import 'package:canton_news_app/src/ui/views/top_stories_view/top_stories_view.dart';
+
+final _topStoriesNavigatorKey = GlobalKey<NavigatorState>();
+final _sourcesNavigatorKey = GlobalKey<NavigatorState>();
+final _categoriesNavigatorKey = GlobalKey<NavigatorState>();
 
 class CurrentView extends StatefulWidget {
   @override
@@ -10,12 +15,24 @@ class CurrentView extends StatefulWidget {
 
 class _CurrentViewState extends State<CurrentView> {
   int _currentIndex = 0;
+
   final List<Widget> _views = [
     TopStoriesView(),
-    TopSourcesView(),
+    SourcesView(),
+    CategoryListView(),
   ];
 
-  void onTabTapped(int index) {
+  void _onTabTapped(int index) {
+    if (index == _currentIndex && _currentIndex == 0 && _topStoriesNavigatorKey.currentState!.canPop()) {
+      _topStoriesNavigatorKey.currentState!.pop();
+    }
+    if (index == _currentIndex && _currentIndex == 1 && _sourcesNavigatorKey.currentState!.canPop()) {
+      _sourcesNavigatorKey.currentState!.pop();
+    }
+    if (index == _currentIndex && _currentIndex == 2 && _categoriesNavigatorKey.currentState!.canPop()) {
+      _categoriesNavigatorKey.currentState!.pop();
+    }
+
     setState(() {
       _currentIndex = index;
     });
@@ -24,8 +41,53 @@ class _CurrentViewState extends State<CurrentView> {
   @override
   Widget build(BuildContext context) {
     return CantonScaffold(
-      bottomNavBar: BottomNavBar(_currentIndex, onTabTapped),
-      body: _views[_currentIndex],
+      backgroundColor: CantonMethods.alternateCanvasColor(context, index: _currentIndex, targetIndexes: [0, 1]),
+      bottomNavBar: BottomNavBar(_currentIndex, _onTabTapped),
+      padding: EdgeInsets.zero,
+      safeArea: false,
+      body: IndexedStack(
+        index: _currentIndex,
+        children: [
+          Navigator(
+            key: _topStoriesNavigatorKey,
+            // observers: [FirebaseAnalyticsObserver(analytics: FirebaseAnalytics())],
+            onGenerateRoute: (settings) {
+              return MaterialPageRoute(
+                settings: settings,
+                fullscreenDialog: true,
+                builder: (context) => SafeArea(child: _views[_currentIndex]),
+              );
+            },
+          ),
+          Navigator(
+            key: _sourcesNavigatorKey,
+            // observers: [FirebaseAnalyticsObserver(analytics: FirebaseAnalytics())],
+            onGenerateRoute: (settings) {
+              return MaterialPageRoute(
+                settings: settings,
+                fullscreenDialog: true,
+                builder: (context) => SafeArea(child: _views[_currentIndex]),
+              );
+            },
+          ),
+          Navigator(
+            key: _categoriesNavigatorKey,
+            // observers: [FirebaseAnalyticsObserver(analytics: FirebaseAnalytics())],
+            onGenerateRoute: (settings) {
+              return MaterialPageRoute(
+                settings: settings,
+                fullscreenDialog: true,
+                builder: (context) => SafeArea(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 17),
+                    child: _views[_currentIndex],
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
